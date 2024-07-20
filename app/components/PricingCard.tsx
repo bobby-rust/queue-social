94eec5362755f9b221ae94023ad58fdee07fb92f
bobby rust <bobby@BobbyDesktop.com>
2024-07-19 23:59:40 -0400

    style: add more features to pricing section

--- app/components/PricingCard.tsx
+++ app/components/PricingCard.tsx
@@ -2,30 +2,39 @@ import { Button } from "@/components/ui/button";
 import { Check } from "lucide-react";
 import React from "react";
 
-export default function PricingCard() {
+interface PricingCardProps {
+	title: string;
+	price: number;
+	features: string[];
+}
+
+export default function PricingCard(props: PricingCardProps) {
 	return (
 		<div className="flex flex-col gap-4 border-2 rounded-lg p-8">
 			<div className="flex flex-col gap-2">
-				<h2 className="text-xl font-semibold">Premium</h2>
+				<h2 className="text-xl font-semibold">{props.title}</h2>
 				<div className="">
-					<span className="text-3xl font-semibold">7.99</span>
+					<span className="text-3xl font-semibold">{props.price}</span>
 					<span className="text-slate-600">/month</span>
 				</div>
 			</div>
 			<div className="flex flex-col gap-2">
 				<h3 className="text-xl">Features</h3>
 				<div>
-					<ul>
-						<li className="flex">
-							<Check /> 5 social media accounts
-						</li>
-						<li className="flex">
-							<Check /> Schedule content up to one week in advance
-						</li>
+					<ul className="flex flex-col gap-2">
+						{props.features.map((feature, i) => {
+							return (
+								<li key={i} className="flex items-center gap-2">
+									<Check /> {feature}
+								</li>
+							);
+						})}
 					</ul>
 				</div>
 			</div>
-			<Button>Get Started</Button>
+			<div className="flex justify-center items-end">
+				<Button className="">Get Started</Button>
+			</div>
 		</div>
 	);
 }
