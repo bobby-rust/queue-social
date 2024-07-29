ef926cea6cbc34dedd376fcff96192cfe370f1e2
bobby rust <bobby.rust121@gmail.com>
2024-07-29 12:20:22 -0400

    style: landing page progress, aggregate home page & landing page into single root page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import NavMenu from "./components/NavMenu";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,12 +19,21 @@ export default async function RootLayout({
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
+	const session = await getServerSession(authOptions);
+
 	return (
 		<html lang="en">
-			<body className={`min-h-screen flex flex-col ${inter.className}`}>
-				<Header />
-				<main className="flex-grow">{children}</main>
-				<Footer />
+			<body className={`${inter.className}`}>
+				<div>
+					{session && <NavMenu />}
+					<div className="grid min-h-[100dvh] grid-rows-[auto_1fr_auto]">
+						<main className="">
+							<Header />
+							{children}
+						</main>
+						<Footer session={session} />
+					</div>
+				</div>
 			</body>
 		</html>
 	);
