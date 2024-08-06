47ec60cf5fd33f9eb287378cfb90b7ddb1bdcd96
bobby rust <bobby.rust121@gmail.com>
2024-08-06 00:12:28 -0400

    text posts to fb working

--- app/api/users/[id]/posts/route.ts
+++ app/api/users/[id]/posts/route.ts
@@ -1,7 +1,26 @@
 export async function POST(request: Request) {
-    return new Response("Hello, Next.js!");
+    const body = await request.json();
+    const META_API_URL = "https://graph.facebook.com/v20.0";
+    console.log(body);
+    // const url = `${META_API_URL}/${body.pageID}/feed?access_token=${body.page.accessToken}&message=${body.content}&link=${body.link}&published=false&scheduled_publish_time=${body.scheduledPublishTime}`;
+    const url = `${META_API_URL}/${body.page.id}/feed?access_token=${body.page.access_token}&message=${body.content}&link=${body.link || ""}`;
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            access_token: body.page.accessToken,
+            message: body.content,
+            link: body.link || "",
+        }),
+    });
+
+    const data = await response.json();
+    console.log(data);
+    return new Response(JSON.stringify(data));
 }
 
 export async function GET(request: Request) {
-    return new Response("Hello, Next.js!");
+    return new Response(JSON.stringify({ message: "Hello, Next.js!" }));
 }
--- app/posts/create/page.tsx
+++ app/posts/create/page.tsx
@@ -4,35 +4,44 @@ import React from "react";
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 import { Link } from "lucide-react";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+// import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+import { useRouter } from "next/router";
 
 const CreatePostSchema = Yup.object().shape({
     content: Yup.string().required("Content is required"),
     image: Yup.mixed(),
     link: Yup.string().url("Invalid URL format"),
-    page: Yup.string().required("Page is required"),
+    page: Yup.string().required("Page is required"), // if the form isnt submitting, make sure page is getting set.
 });
 
 interface Post {
     content: string;
-    image: string;
+    image: any;
     link: string;
-    page: string;
+    page: any;
 }
 
 export default function CreatePost() {
-    const session = useSession();
-
-    const pages = session.data?.user.facebook_business_accounts.pages;
+    const session: any = useSession();
+    const pages = session.data?.user?.facebook_business_accounts.pages;
+    const router = useRouter();
     console.log(pages);
 
     const createPost = async (post: Post) => {
-        console.log("HEelloooo");
+        if (post.page === "default") {
+            alert("Please select a page to post to");
+        }
         console.log(post);
-        return;
-        const res = await fetch("/api/posts", {
+        for (const page of pages.data) {
+            if (page.name === post.page) {
+                post.page = page;
+            }
+        }
+
+        const res = await fetch("/api/users/1/posts", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -40,6 +49,8 @@ export default function CreatePost() {
             body: JSON.stringify(post),
         });
         const data = await res.json();
+        console.log(data);
+        router.push("/");
     };
 
     return (
@@ -47,16 +58,21 @@ export default function CreatePost() {
             <div className="flex flex-col gap-4 justify-center items-center bg-base-200 p-12 rounded-lg shadow-lg w-3/5">
                 <h1 className="text-3xl bold">Create Post</h1>
                 <Formik
-                    initialValues={{ content: "", image: "", link: "", page: "" }}
+                    initialValues={{
+                        content: "",
+                        image: "",
+                        link: "",
+                        page: "default",
+                    }}
                     validationSchema={CreatePostSchema}
                     onSubmit={(values, { setSubmitting }) => {
                         createPost(values);
                         setSubmitting(false);
                     }}
                 >
-                    {({ values, setFieldValue, errors, touched }) => (
+                    {({ handleSubmit, values, setFieldValue, errors, touched }) => (
                         <Form
-                            onChange={(e) => console.log(e.target.value)}
+                            onChange={(e: any) => console.log(e.target.value)}
                             className="flex flex-col gap-4 w-full"
                         >
                             <div className="form-control w-full min-h-80">
@@ -154,13 +170,13 @@ export default function CreatePost() {
                                                 Add an image to your post (optional)
                                             </span>
                                         </label>
-                                        <input
+                                        <Field
                                             id="image"
                                             name="image"
                                             type="file"
                                             placeholder="Image"
                                             className="file-input file-input-bordered"
-                                            onChange={(e) => {
+                                            onChange={(e: any) => {
                                                 setFieldValue("image", e.target.files?.[0]);
                                             }}
                                             accept="image/*"
@@ -170,18 +186,23 @@ export default function CreatePost() {
                             </div>
                             <Field
                                 as="select"
+                                component="select"
                                 name="page"
                                 id="page"
-                                className="select select-bordered w-full max-w-xs"
+                                className={`select select-bordered w-full max-w-xs`}
                             >
-                                <option disabled selected>
+                                <option value="default" disabled selected>
                                     Which page would you like to post to?
                                 </option>
-                                {pages.data.map((page) => (
+                                {pages.data.map((page: any) => (
                                     <option key={page.id}>{page.name}</option>
                                 ))}
                             </Field>
-                            <button type="submit" className="btn btn-primary w-1/5">
+                            <button
+                                type="submit"
+                                className="btn btn-primary w-1/5"
+                                onClick={(e: any) => handleSubmit(e)}
+                            >
                                 Submit
                             </button>
                         </Form>
