8cfe9f4cae43a908c830040a0cad269aca24b3eb
bobby rust <bobby.rust121@gmail.com>
2024-08-28 13:37:11 -0400

    remove unused folders from old ui libraries

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,6 @@ import { withUt } from "uploadthing/tw";
 
 const config: Config = withUt({
     content: [
-        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./components/**/*.{js,ts,jsx,tsx,mdx}",
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
         "./styles/*",
